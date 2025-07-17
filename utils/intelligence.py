7b39d80a211d98780e8d3b453893f95cba497023
5leepy <n4deer@gmail.com>
2025-07-17 23:07:40 +0700

    feat: Adapt get_intelligence_detail for universal parent questions
    Memperbarui fungsi  untuk mengambil data pertanyaan orang tua (ortu) dan skor maksimalnya langsung dari tingkat kecerdasan.

--- utils/intelligence.py
+++ utils/intelligence.py
@@ -9,4 +9,25 @@ from bank_soal import BANK_SOAL as INTELLIGENCE_DETAILS
 INTELLIGENCE_NAMES_ORDERED = list(INTELLIGENCE_DETAILS.keys())
 
 def get_intelligence_detail(kecerdasan, jenjang):
-    return INTELLIGENCE_DETAILS.get(kecerdasan, {}).get(jenjang, {})
+    """
+    Mengambil detail kecerdasan untuk jenjang tertentu.
+    Data 'ortu' dan 'max_ortu_score' kini diambil langsung dari tingkat kecerdasan,
+    bukan dari dalam jenjang.
+    """
+    detail = INTELLIGENCE_DETAILS.get(kecerdasan, {})
+    
+    # Ambil data anak dari jenjang spesifik
+    anak_data = detail.get(jenjang, {}).get("anak", [])
+    max_anak_score = detail.get(jenjang, {}).get("max_anak_score", 0)
+
+    # Ambil data ortu dan max_ortu_score langsung dari tingkat kecerdasan
+    ortu_data = detail.get("ortu", [])
+    max_ortu_score = detail.get("max_ortu_score", 0)
+
+    return {
+        "anak": anak_data,
+        "ortu": ortu_data,
+        "max_anak_score": max_anak_score,
+        "max_ortu_score": max_ortu_score
+    }
+
